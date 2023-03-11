804dba4532b27d118b7e07b7332cd0393852cad0
mikygaleano <creatormikeonline@gmail.com>
2023-03-11 19:29:28 -0300

    Sobre mí

--- src/component/SobreMi/SobreMi.jsx
+++ src/component/SobreMi/SobreMi.jsx
@@ -3,7 +3,20 @@ import style from './SobreMi.module.css'
 export default function SobreMi () {
     return (
         <section id='sobreMi' className={style.sobreMi}>
-            Hola
+            <div className={style.card}>
+                <figure className={style.cardImg}>
+                    <img src="./image/perfil.png" alt="" />
+                </figure>
+                <article className={style.info}>
+                    <span>Michael Galeano</span>
+                    <p>
+                        developer junior Y estudiante <br />
+                        de ingeneria en informatica <br />
+                        en la Universidad Nacional de Avellaneda <br />
+                        desde el 2023. 
+                    </p>
+                </article>
+            </div>
         </section>
     )
 }
